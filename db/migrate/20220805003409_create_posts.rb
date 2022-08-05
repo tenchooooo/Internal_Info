bea6e4cd5d77cbd8266ffc713c0159a488b1976b
tenchooooo <tenchooo.227@gmail.com>
2022-08-05 00:54:04 +0000

    post作成

---
+++ db/migrate/20220805003409_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :member_id, null: false
+      t.string :subject, null: false
+      t.text :text, null: false
+      t.string :check_count, null: false
+      t.datetime :limit, null: false
+      t.boolean :browse_status, null: false, default: true
+      t.timestamps
+    end
+  end
+end
