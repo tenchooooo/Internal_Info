9493c8c3119f4847bf9a484c39a4af28f6953ae7
tenchooooo <tenchooo.227@gmail.com>
2022-09-03 12:38:42 +0000

    adminmember

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,4 +2,25 @@ class Admin::MembersController < ApplicationController
   def index
     @members = Member.all
   end
+  
+  def show
+    @member = Member.find(params[:id])
+  end
+  
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to admin_member_path(@member.id), success: t('投稿完了しました。')
+    else
+      render :edit
+    end
+  end
+  
+  def member_params
+    params.require(:member).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :telephone_number, :department)
+  end
 end
