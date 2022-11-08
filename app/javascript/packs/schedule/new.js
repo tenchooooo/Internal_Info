0e8a89698fa5bda235a688008f567ad57aa26af0
tenchooooo <tenchooo.227@gmail.com>
2022-11-08 07:26:16 +0000

    JavaScript練習

--- app/javascript/packs/schedule/new.js
+++ app/javascript/packs/schedule/new.js
@@ -1,1 +1,12 @@
-alert("gggg");
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA) {
+  let addStr = "Hello" + strA;
+  return addStr;
+}
