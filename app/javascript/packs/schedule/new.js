58899f7fd7fd8a1eec87e31cd00dc19a9ac398db
tenchooooo <tenchooo.227@gmail.com>
2022-11-13 14:00:48 +0000

    childrenのお勉強

--- app/javascript/packs/schedule/new.js
+++ app/javascript/packs/schedule/new.js
@@ -42,6 +42,18 @@
 
 // 「children」は、HTML要素直下のすべての子要素を取得するときに使います。
 
+$(function(){
+  $('button').on('click', function(){
+    // 「button要素がクリックされたときに、処理を実行してください」
+    
+    $('ul').children().css('color', 'red');
+    // .children()は、HTML要素直下のすべての子要素を取得します。
+    // ここでのchildrenは、「ul要素の子要素」、つまり、li要素が対象になります。
+  });
+});
+
+
+
 
 
 
