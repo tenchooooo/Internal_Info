6e8cb6a22b461967b65aa9f59d581379c5700d01
tenchooooo <tenchooo.227@gmail.com>
2022-11-14 04:04:01 +0000

    トップに戻るをつくってみた

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import "../stylesheets/calendar"
-import "../stylesheets/style"
+import "../stylesheets/schedule/style"
 
 Rails.start()
 Turbolinks.start()
