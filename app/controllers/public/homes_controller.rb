70651a17e363321e24140a277bf3bc3b29134545
tenchooooo <tenchooo.227@gmail.com>
2022-11-16 02:15:49 +0000

    helpermethodno ちょっとした不具合

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,12 @@
 class Public::HomesController < ApplicationController
   before_action :authenticate_member!, except: [:top]
   before_action :member_is_approval
+
+  helper_method :grouping_attendance
+  helper_method :grouping_trip
+  helper_method :grouping_go_out
+  helper_method :grouping_other
+
   def member_is_approval
     if current_member.is_approval != true
       redirect_to  member_path(current_member.id)
@@ -21,4 +27,47 @@ class Public::HomesController < ApplicationController
       @day = schedule.day
     end
   end
+
+  def grouping_attendance(schedules, attendance)
+    total = 0
+    schedules.each do |schedule|
+      if schedule.attendance == attendance
+        total = total + 1
+      end
+    end
+    return total
+
+  end
+
+  def grouping_trip(schedules)
+    total = 0
+    schedules.each do |schedule|
+      if schedule.trip.present?
+        total = total + 1
+      end
+    end
+    return total
+  end
+
+  def grouping_go_out(schedules)
+    total = 0
+    schedules.each do |schedule|
+      if schedule.go_out.present?
+        total = total + 1
+      end
+    end
+    return total
+  end
+
+  def grouping_other(schedules)
+    other_total = 0
+    schedules.each do |schedule|
+      if schedule.other.present?
+        other_total = other_total + 1
+      end
+    end
+    return other_total
+  end
+
+
 end
