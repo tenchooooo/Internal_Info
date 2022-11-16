b33964b3aa539514ce243f854a4fee00419131c3
tenchooooo <tenchooo.227@gmail.com>
2022-11-16 07:55:52 +0000

    jQuery勉強中

--- app/javascript/packs/schedule/new.js
+++ app/javascript/packs/schedule/new.js
@@ -1,1 +1,17 @@
 /*global $*/
+
+$('#box-contents .box[id != "box1"]').hide();
+$(function(){
+  $('[name=schedule[select_schedule]:radio').change( function(){
+    if($('[id="schedule_select_schedule_1"]').prop("checked")){
+      $('.box').fadeOut();
+      $('.box1').fadeIn();
+    } else if($('[id="schedule_select_schedule_2"]').prop("checked")){
+      $('.box').fadeOut();
+      $('.box2').fadeIn();
+    } else if($('[id="schedule_select_schedule_3"]').prop("checked")){
+      $('.box').fadeOut();
+      $('.box3').fadeIn();
+    }
+  });
+});
