1bb208b697940f0389fbfbb2372cb56a68f71200
tenchooooo <tenchooo.227@gmail.com>
2022-11-26 06:11:54 +0000

    JavaScript修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application"
 import "../stylesheets/calendar"
 import "../stylesheets/schedule/new"
+import "../packs/schedule/new"
 
 Rails.start()
 Turbolinks.start()
